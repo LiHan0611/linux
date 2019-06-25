3d4a5cc14808f699c89ce5bf44fb2a7de0ccd083
cathowlet <33780750+cathowlet@users.noreply.github.com>
2019-06-25 10:05:56 +0800

    Add files via upload

--- seafile/docker_by/seafile_default_db.sh
+++ seafile/docker_by/seafile_default_db.sh
@@ -6,3 +6,16 @@ sudo yum update -y \
 && sudo systemctl start docker \
 && sudo systemctl enable docker
 
+read -p "input your IP address or dominname: " SEAFILE_SERVER_HOSTNAME
+read -p "input administrator acount e-mail: " SEAFILE_ADMIN_EMAIL
+read -p "input administartor password: " SEAFILE_ADMIN_PASSWORD
+
+docker run -d --name seafile \
+  -e SEAFILE_SERVER_HOSTNAME=${SEAFILE_SERVER_HOSTNAME} \
+  -e SEAFILE_ADMIN_EMAIL=${SEAFILE_ADMIN_EMAIL} \
+  -e SEAFILE_ADMIN_PASSWORD=${SEAFILE_ADMIN_PASSWORD} \
+  -v /opt/seafile-data:/shared \
+  -p 80:80 \
+  seafileltd/seafile:latest
+
+
