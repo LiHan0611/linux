ca3d567e7fd46edace8277045ab1e2b2c1fe6867
Your Name <you@example.com>
2019-07-02 21:49:31 +0800

    frp

--- seafile/docker_by/docker_run_tempalte.sh
+++ seafile/docker_by/docker_run_tempalte.sh
@@ -1,3 +1,8 @@
+read -p "input your ip address or dominname" SEAFILE_SERVER_HOSTNAME
+read -p "input your ip address or dominname" SEAFILE_ADMIN_EMAIL
+read -p "input your ip address or dominname" SEAFILE_ADMIN_PASSWORD
+
+
 docker run -d --name seafile \
   -e SEAFILE_SERVER_HOSTNAME=${SEAFILE_SERVER_HOSTNAME} \
   -e SEAFILE_ADMIN_EMAIL=${SEAFILE_ADMIN_EMAIL} \
