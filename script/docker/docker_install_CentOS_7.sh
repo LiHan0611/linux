31b329662af8c84f012670aa7f5c3ccfe20592bd
LiHan0611 <36853338+LiHan0611@users.noreply.github.com>
2019-07-18 00:08:31 +0800

    Update docker_install_CentOS_7.sh

--- script/docker/docker_install_CentOS_7.sh
+++ script/docker/docker_install_CentOS_7.sh
@@ -6,3 +6,6 @@ sudo yum update
 && sudo systemctl start docker
 && sudo systemctl enable docker
 
+# docker-compose
+&& curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+&& chmod +x /usr/local/bin/docker-compose
